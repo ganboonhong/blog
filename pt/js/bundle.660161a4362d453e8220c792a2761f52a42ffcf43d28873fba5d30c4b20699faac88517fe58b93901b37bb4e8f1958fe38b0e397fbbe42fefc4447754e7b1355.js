2bcae2ae144a0d3737c4377cfa2b892ba77b9355
ganboonhong <ganboonhong@users.noreply.github.com>
2022-09-29 07:43:31 +0000

    deploy: e7f1e882b904dfb1ca0de347f46233b83489e793

--- pt/js/bundle.660161a4362d453e8220c792a2761f52a42ffcf43d28873fba5d30c4b20699faac88517fe58b93901b37bb4e8f1958fe38b0e397fbbe42fefc4447754e7b1355.js
+++ pt/js/bundle.660161a4362d453e8220c792a2761f52a42ffcf43d28873fba5d30c4b20699faac88517fe58b93901b37bb4e8f1958fe38b0e397fbbe42fefc4447754e7b1355.js
@@ -8,7 +8,7 @@ const scaleImageClass = 'image-scale';
 const pageHasLoaded = 'DOMContentLoaded';
 const imageAltClass = 'img_alt'
 
-const baseURL = 'https://ganboonhong.github.io/refs';
+const baseURL = 'https://ganboonhong.github.io/refs/';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
 const goBackClass = 'button_back';
